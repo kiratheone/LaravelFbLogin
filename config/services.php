f049cd4eebb04d3b8a085eef38777cf51ba4446d
Arik Achmad <arikarik.ae@gmail.com>
2016-03-14 18:38:59 +0700

    penambahan konfigurasi service untuk facebook

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'facebook' => [
+        'client_id' => env('FB_CLIENT_ID','590438687772709'),
+        'client_secret' => env('FB_CLIENT_SECREET_ID','5f852d5bef58c1c6d35805c50af9bf7a'),
+        'redirect' => env('REDIRECT_URL','http://localhost/'),
+    ],
+
 ];
