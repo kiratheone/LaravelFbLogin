43ae9e5b25ac8d99f1e265b195b2e1b6bf5ee225
Arik Achmad <arikarik.ae@gmail.com>
2016-03-16 17:32:42 +0700

    Penambahan Try Catch untuk handling ketika user membatalkan ijin aplikasi

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -6,11 +6,16 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Socialite;
+use Exception;
 class Home extends Controller
 {
     public function index()
     {
-    	$user = Socialite::driver('facebook')->user();
-    	dd($user);
+    	try {
+    		$user = Socialite::driver('facebook')->user();
+    		dd($user);
+    	} catch (Exception $e) {
+    		return "ohh meeen. you cannot doing this";
+    	}    	
     }
 }
