352a1bb2f354ae9267d47cfc7f577ee724121be9
Arik Achmad <arikarik.ae@gmail.com>
2016-03-17 18:58:07 +0700

    code beauty formatter

--- resources/views/LoginFbView.blade.php
+++ resources/views/LoginFbView.blade.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Login with Facebook</title>
-	<link href="{{{ asset('/res/style.css') }}}" rel="stylesheet" type="text/css">
-</head>
-<body>
-	<div>
-		<form>
-			<h2>Helo login</h2>
-			<button>Connect</button>
-		</form>
-	</div>
-</body>
+    <head>
+        <title>
+            Login with Facebook
+        </title>
+        <link href="{{{ asset('/res/style.css') }}}" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div>
+            <form>
+                <h2>
+                    Helo login
+                </h2>
+                <button>
+                    Connect
+                </button>
+            </form>
+        </div>
+    </body>
 </html>
