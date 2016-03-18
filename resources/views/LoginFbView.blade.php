8f3e69658164b014e2b64c48389c2cb8fa964f21
Arik Achmad <arikarik.ae@gmail.com>
2016-03-18 08:35:42 +0700

    ganti font menggunakan roboto

--- resources/views/LoginFbView.blade.php
+++ resources/views/LoginFbView.blade.php
@@ -4,6 +4,7 @@
         <title>
             Login with Facebook
         </title>
+        <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
         <link href="{{{ asset('/css/style.css') }}}" rel="stylesheet" type="text/css"/>
     </head>
     <body>
