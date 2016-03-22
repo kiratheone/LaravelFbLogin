89376a570cd1f6509b7ab9a3bc2c740bb51e7da7
Arik Achmad <arikarik.ae@gmail.com>
2016-03-23 05:40:07 +0700

    memungkinkan register dan login secara otomatis setelah melakukan login facebook dengan menggunakan auth laravel

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Socialite;
+use App\User;
+Use Auth;
 
 class FacebookController extends Controller
 {
@@ -17,5 +19,16 @@ class FacebookController extends Controller
 	public function callback()
 	{
 	    $user = Socialite::driver('facebook')->user();
+	    //User::create(['name'=>$user->getName(), 'email'=>$user->getEmail()]);
+	    $userCheck = User::whereEmail([$user->getEmail()])->first();
+	    // dd($userCheck);
+	    if ($userCheck) {
+	    	Auth::login($userCheck);
+	    	return redirect('home');
+	    } else {
+	    	$user = User::create(['name'=>$user->getName(), 'email'=>$user->getEmail()]);
+	    	Auth::login($user);
+	    	return redirect('home');
+	    }
 	}
 }
