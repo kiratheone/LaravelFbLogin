3b5c6e81a40aadbf54050bcde1f8dc528adbc1af
Arik Achmad <arikarik.ae@gmail.com>
2016-03-31 08:24:48 +0700

    pembuatan parent view

---
+++ resources/views/layouts/parent.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Login with Facebook
+        </title>
+        <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+        <link href="{{{ asset('/css/style.css') }}}" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
