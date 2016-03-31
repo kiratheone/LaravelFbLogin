f29d77182f1bc64bb0b543062fd1b7e31b279e0e
Arik Achmad <arikarik.ae@gmail.com>
2016-03-31 08:23:17 +0700

    implementasi penggunaan blade pada laman login fb

--- resources/views/LoginFbView.blade.php
+++ resources/views/LoginFbView.blade.php
@@ -1,20 +1,12 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>
-            Login with Facebook
-        </title>
-        <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-        <link href="{{{ asset('/css/style.css') }}}" rel="stylesheet" type="text/css"/>
-    </head>
-    <body>
-        <div class="login-container">
-            <form class="form-login">
-                <h2>
-                    Login
-                </h2>
-                <a class="btn-fb-login" href="{{ url('/login/facebook')}}">Login with Facebook</a>
-            </form>
-        </div>
-    </body>
-</html>
+@extends('layouts.parent')
+
+@section('content')
+    <div class="login-container">
+        <form class="form-login">
+            <h2>
+                Login
+            </h2>
+        <a class="btn-fb-login" href="{{ url('/login/facebook')}}">Login with Facebook</a>
+        </form>
+    </div>
+@endsection
